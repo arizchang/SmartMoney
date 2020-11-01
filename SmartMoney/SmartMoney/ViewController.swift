81373522e29747e115ca00d179c87a74ac70a758
Ariz Chang <abchang@asu.edu>
2020-11-01 11:58:09 -0700

    first commit

---
+++ SmartMoney/SmartMoney/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartMoney
+//
+//  Created by Ariz Chang on 11/1/20.
+//  Copyright © 2020 Ariz Chang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
