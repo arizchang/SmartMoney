6bfcc31441d998da247adefbf2e907823b99c806
Ariz Chang <abchang@asu.edu>
2020-11-01 15:24:12 -0700

    finished adding view controllers

---
+++ SmartMoney/SmartMoney/PaymentsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PaymentsViewController.swift
+//  SmartMoney
+//
+//  Created by Ariz Chang on 11/1/20.
+//  Copyright © 2020 Ariz Chang. All rights reserved.
+//
+
+import UIKit
+
+class PaymentsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
