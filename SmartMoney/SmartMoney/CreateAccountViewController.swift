c0814cfb03901fe7e323e769ea9a8ac41275b16b
Ariz Chang <abchang@asu.edu>
2020-11-20 16:57:58 -0700

    added image picker

--- SmartMoney/SmartMoney/CreateAccountViewController.swift
+++ SmartMoney/SmartMoney/CreateAccountViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class CreateAccountViewController: UIViewController {
+class CreateAccountViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
     @IBOutlet weak var usernameField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
@@ -18,6 +18,22 @@ class CreateAccountViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         profilePic.image = UIImage(named: "blankProfilePic.png")
+    }
+    
+    
+    @IBAction func changeProfilePic(_ sender: UIButton) {
+        let photoPicker = UIImagePickerController()
+        photoPicker.delegate = self
+        photoPicker.sourceType = .photoLibrary
+        self.present(photoPicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
+        
+        picker .dismiss(animated: true, completion: nil)
+
+        let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        profilePic.image = UIImage(data: image!.pngData()! as Data)
     }
     
 }
